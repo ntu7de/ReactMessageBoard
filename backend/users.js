e950291beb8950b83a76b44b69ace3ad0d0340b4
Patrick Yuson <ntu7de@virginia.edu>
2024-05-27 15:43:20 -0400

    added the database imports to users.js

--- backend/users.js
+++ backend/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const db = require('./firebase');
+const { collection, getDocs, updateDoc, doc, addDoc } = require('firebase/firestore');
+
 router.get('/', (req, res) => {
     let ret = [];
     // Get the data
