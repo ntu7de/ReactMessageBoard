a12b1d74c856c8989affd6830951d1479ae406fd
Patrick Yuson <ntu7de@virginia.edu>
2024-05-27 15:43:41 -0400

    Beginning frontend for read functionality

--- frontend/message-board-app/src/components/MessageBoard.jsx
+++ frontend/message-board-app/src/components/MessageBoard.jsx
@@ -4,11 +4,13 @@ import '../styles/MessageBoard.css'
 
 const MessageBoard = () => {
     const [message, setMessage] = useState("");
+    const [username, setUsername] = useState("");
     const [allMessages, setAllMessages] = useState([]);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         const body = {
+            username: username,
             message: message,
         }
         // add axios response
@@ -16,9 +18,13 @@ const MessageBoard = () => {
 
     // CRUD: Read
     const fetchMessages = async () => {
-        const response = await axios.get("http://localhost:5005/messages");
-        console.log('response', response.data);
-        setAllMessages(response.data);
+        try {
+            const response = await axios.get("http://localhost:8000/messages");
+            console.log('response', response.data);
+            setAllMessages(response.data);
+        } catch (error) {
+            console.log("Error fetching messages: ", error)
+        }
     }
 
     useEffect(() => {
@@ -28,17 +34,24 @@ const MessageBoard = () => {
     return (
         <>
             <h2>Welcome to your message board!</h2>
-            {/* <div className='input-container'> */}
-            <form onSubmit={handleSubmit}>
-                <p>Share your thoughts here: </p>
-                <input 
-                    type='text' 
-                    className='input'
-                    onChange={(e) => setMessage(e.target.value)}></input>
-                <br></br>
-                <button type='submit'>Submit</button>
-            </form>
-            {/* </div> */}
+            <div className='input-container'>
+                <form onSubmit={handleSubmit}>
+                    <p>What's your username?</p>
+                    <input 
+                        className='username-input'
+                        onChange={(e) => setUsername(e.target.value)}></input>
+                    <p>Share your thoughts here: </p>
+                    <input 
+                        type='text' 
+                        className='message-input'
+                        onChange={(e) => setMessage(e.target.value)}></input>
+                    <br></br>
+                    <button type='submit'>Submit</button>
+                </form>
+            </div>
+            <div className='previous-messages'>
+                <p>Previous messages will go here.</p>
+            </div>
         </>
     )
 }
