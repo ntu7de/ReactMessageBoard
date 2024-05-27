a560d2e762a1cd0a676d7de8e023cf47309a9460
Patrick Yuson <ntu7de@virginia.edu>
2024-05-27 10:43:31 -0400

    Initial setting up of front end before implementing backend

---
+++ frontend/message-board-app/src/App.jsx
@@ -0,0 +1,13 @@
+import MessageBoard from './components/MessageBoard'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <MessageBoard />
+    </>
+  )
+}
+
+export default App
---
+++ frontend/message-board-app/src/components/MessageBoard.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import '../styles/MessageBoard.css'
+
+const MessageBoard = () => {
+    const [message, setMessage] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const body = {
+            message: message,
+        }
+        // add axios response
+    }
+
+    return (
+        <>
+            <h2>Welcome to your message board!</h2>
+            <div className='input-container'>
+                <p>Share your thoughts here: </p>
+                <input 
+                    type='text' 
+                    className='input'
+                    onChange={(e) => setMessage(e.target.value)}></input>
+                <br></br>
+                <button type='submit'>Submit</button>
+            </div>
+        </>
+    )
+}
+
+export default MessageBoard
